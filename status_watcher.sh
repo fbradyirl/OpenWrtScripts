72b0ddd65ad4018ba66d16412a904b55ae04ae1c
Finbarr Brady <fbradyirl@users.noreply.github.com>
2016-05-18 18:34:22 +0100

    Fix call to openvpn-status

--- status_watcher.sh
+++ status_watcher.sh
@@ -14,17 +14,17 @@ SLEEPTIME=30
 # Assuming this is run on boot, the status files will be 
 # empty. Storing this, so that we can see when
 # all connections are gone.
-ZERO_CONNECTIONS_HASH="$(sh openvpn-status.sh | md5sum)"
+ZERO_CONNECTIONS_HASH="$(openvpn-status.sh | md5sum)"
 OLD_CONNECTIONS_HASH=''
 while true; do
-	NEW_CONNECTIONS_HASH="$(sh openvpn-status.sh | md5sum)"
+	NEW_CONNECTIONS_HASH="$(openvpn-status.sh | md5sum)"
 	if [ "$NEW_CONNECTIONS_HASH" == "$ZERO_CONNECTIONS_HASH" ] ; then
                 echo "Zero connection detected"
                 echo "Sleeping for $SLEEPTIME seconds..."
                 sleep $SLEEPTIME		
 	elif [ "$NEW_CONNECTIONS_HASH" != "$OLD_CONNECTIONS_HASH" ] ; then
 		echo "New connection detected"
-		push.sh "$HOSTNAME VPN Update" "$(sh openvpn-status.sh)"
+		push.sh "$HOSTNAME VPN Update" "$(openvpn-status.sh)"
 		OLD_CONNECTIONS_HASH="$NEW_CONNECTIONS_HASH"
 	else
 		echo "No updates to openvpn status file."
